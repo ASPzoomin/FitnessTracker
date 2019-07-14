efc55f92cbfefd51cd9bb354c0f54c9787150900
pavan <pavanammula@gmail.com>
2019-07-14 19:20:31 +0530

    Routing and authentication

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { AuthService } from './auth/auth.service';
 import { Subscription } from 'rxjs/Subscription';
+import { MatSidenav } from '@angular/material';
 
 @Component({
   selector: 'app-root',
@@ -16,6 +17,10 @@ export class AppComponent implements OnInit, OnDestroy {
     this.authSubscription=this.authService.authChange.subscribe(authStatus =>{
       this.isAuth = authStatus;
     })
+  }
+
+  onLogout() {
+    this.authService.logout();
   }
 
   ngOnDestroy() {
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,16 +1,20 @@
 import { User } from './user.model';
 import { AuthData } from './auth-data.model';
 import { Subject } from 'rxjs/Subject';
-
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+@Injectable()
 export class AuthService {
     private user: User;
     authChange = new Subject<boolean>();
+    constructor(private route:Router){}
     registeredUser(authData: AuthData) {
         this.user ={
             email:authData.email,
             userId:Math.round(Math.random()*10000).toString()
         };
         this.authChange.next(true);
+        this.route.navigate(['/training'])
     }
     login(authData: AuthData) {
         this.user ={
@@ -18,10 +22,12 @@ export class AuthService {
             userId:Math.round(Math.random()*10000).toString()
         };
         this.authChange.next(true);
+        this.route.navigate(['/training'])
     }
     logout() {
         this.user = null;
         this.authChange.next(false);
+        this.route.navigate(['/login'])
     }
     getUser() {
         return {...this.user};
